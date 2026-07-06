a0734c3b8f3bed48635d54242b1779eb94973211
joako perez <8bussy8@gmail.com>
2026-07-05 22:10:59 -0400

    02

--- menu-service-quickbite/src/main/java/com/proyecto/menu/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.menu.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String mensaje) {
-        super(mensaje);
-    }
-
-}
