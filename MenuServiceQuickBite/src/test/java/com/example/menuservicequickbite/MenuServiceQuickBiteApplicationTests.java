cce56a14769eeaf670a95438ec1a7ea91ec0efd6
joako perez <8bussy8@gmail.com>
2026-07-06 23:28:06 -0400

    05.1

---
+++ MenuServiceQuickBite/src/test/java/com/example/menuservicequickbite/MenuServiceQuickBiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.menuservicequickbite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuServiceQuickBiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
